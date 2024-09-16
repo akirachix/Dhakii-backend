9f799e413dcfd9b615fd3b3d88bc0e77e43ad5ce
Shalom-uwamahoro <shalomuwamahoro@gmail.com>
2024-09-16 10:12:48 +0300

    removed unnecessary comments

--- mamamind/nurse_admin/models.py
+++ mamamind/nurse_admin/models.py
@@ -5,31 +5,6 @@ from django.conf import settings
 from django.core.validators import RegexValidator
 
 
-# class NurseAdmin(models.Model):
-#     """
-#     NurseAdmin Model - Stores information about nurse administrators.
-#     Fields:
-#         - admin_id: Primary key, unique for each nurse admin.
-#         - hospital_id: Foreign key to the Hospital model.
-#         - user_id: Foreign key to the User model.
-#         - location: Location of the nurse admin.
-#         - sub_location: Sub-location of the nurse admin.
-#         - created_at: Auto-set field for record creation timestamp.
-#         - updated_at: Auto-set field for record update timestamp.
-#     """
-
-#     admin_id = models.AutoField(primary_key=True)
-#     hospital_id = models.ForeignKey(Hospital, on_delete=models.CASCADE)
-#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-#     tel_no = models.CharField(max_length=15)
-#     location = models.CharField(max_length=255)
-#     sub_location = models.CharField(max_length=255)
-
-
-#     def __str__(self):
-#         return f"Nurse Admin {self.user.username} ({self.hospital_id.name})"
 class NurseAdmin(models.Model):
     """
     NurseAdmin Model - Stores information about nurse administrators.
--- mamamind/screeningtestscore/models.py
+++ mamamind/screeningtestscore/models.py
@@ -1,15 +1,3 @@
-# from django.db import models
-
-# class ScreeningTestScore(models.Model):
-#     test_id = models.AutoField(primary_key=True)  # Unique ID for each test
-#     mother= models.ForeignKey('mother.Mother', on_delete=models.CASCADE)
-#     chp= models.ForeignKey('community_health_promoter.CHP', on_delete=models.CASCADE)
-#     test_date = models.DateField()
-#     total_score = models.PositiveSmallIntegerField()
-
-#     def __str__(self):
-#         return f"Test {self.test_id} - Total Score: {self.total_score}"
-
 from django.db import models
 from django.core.exceptions import ValidationError
 from datetime import date
