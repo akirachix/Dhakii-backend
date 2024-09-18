57d1400e81a17893cf2c88545f1bc9db72cfb5fb
Yaar-Nuul <yaarnuulreech@gmail.com>
2024-09-18 14:33:34 +0300

    Update the setting.py file

--- mamamind/mamamind/settings.py
+++ mamamind/mamamind/settings.py
@@ -133,12 +133,19 @@ import os
 # }
 
 
-
+# Database configure
 
 DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
+if os.getenv('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+else:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 
 
 # Password validation
