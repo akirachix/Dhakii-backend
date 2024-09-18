81b67556bde6b8d73c7e5134c5ccfbd750c947dd
Yaar-Nuul <yaarnuulreech@gmail.com>
2024-09-18 21:45:29 +0300

    Made changes

--- mamamind/settings.py
+++ mamamind/settings.py
@@ -228,15 +228,13 @@ REST_FRAMEWORK = {
 }
 
 
+AUTH0_CLIENT_ID = os.getenv('AUTH0_CLIENT_ID',"")
+AUTH0_CLIENT_SECRET = os.getenv('AUTH0_CLIENT_SECRET',"")
+AUTH0_DOMAIN = os.getenv('AUTH0_DOMAIN',"")
 
-AUTH0_CLIENT_ID = os.getenv('AUTH0_CLIENT_ID')
-AUTH0_CLIENT_SECRET = os.getenv('AUTH0_CLIENT_SECRET')
-AUTH0_DOMAIN = os.getenv('AUTH0_DOMAIN')
 
 
-REDIRECT_URI = 'http://localhost:8000/auth/callback/'
-REDIRECT_URI = 'http://localhost:8000/auth/'
-
+REDIRECT_URI = os.getenv('REDIRECT_URI', 'http://localhost:8000/auth/callback/')
 
 
 SIMPLE_JWT = {
