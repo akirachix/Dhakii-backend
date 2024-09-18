ffda0dbaacb1dfaa14882486fb081ff2c2bc17f3
Yaar-Nuul <yaarnuulreech@gmail.com>
2024-09-18 20:53:01 +0300

    Made changes

--- mamamind/settings.py
+++ mamamind/settings.py
@@ -220,18 +220,6 @@ if ENV_FILE:
 
 
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_RENDERER_CLASSES': (
-#         'rest_framework.renderers.JSONRenderer',
-#     ),
-#     'DEFAULT_PARSER_CLASSES': (
-#         'rest_framework.parsers.JSONParser',
-#     ),
-
-#     'DEFAULT_AUTHENTICATION_CLASSES': [
-#         'rest_framework_simplejwt.authentication.JWTAuthentication',
-#     ],
-# }
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_AUTHENTICATION_CLASSES': (
