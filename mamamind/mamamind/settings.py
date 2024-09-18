3e160b7ecf64b0ae554e73ddb8cf53cab22badc1
Yaar-Nuul <yaarnuulreech@gmail.com>
2024-09-18 14:30:54 +0300

    Update the setting.py file

--- mamamind/mamamind/settings.py
+++ mamamind/mamamind/settings.py
@@ -132,13 +132,6 @@ import os
 #     }
 # }
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, 'db.sqlite3')
-
-    }
-}
 
 
 
